588c34b6fc74159c524ca06bdcd91ffb76e72b03
leroy <leroy.kariuki@student.moringaschool.com>
2023-07-31 08:42:16 +0300

    app.js

--- bot-app/src/App.js
+++ bot-app/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [bots, setBots] = useState([]);
+  const [BotArmy, setBotArmy] = useState([]);
+
+  useEffect(() => {
+    fetch('/db.json')
+      .then(response => response.json())
+      .then(data => setBots(data.bots))
+      .catch(error => console.error(error));
+  }, []);
+
+  const addBotToArmy = (bot) => {
+    if (!BotArmy.includes(bot)) {
+      setBotArmy([...BotArmy, bot]);
+    }
+  };
+
+  const removeBotFromArmy = (bot) => {
+    setBotArmy(BotArmy.filter(b => b.id !== bot.id));
+  };
+
+  const dischargeBot = (bot) => {
+    fetch(`/bots/${bot.id}`, {
+      method: 'DELETE'
+    })
+    .then(() => {
+      setBots(bots.filter(b => b.id !== bot.id));
+      setBotArmy(BotArmy.filter(b => b.id !== bot.id));
+    })
+    .catch(error => console.error(error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bot Collection</h1>
+      <BotCollection bots={bots} addBotToArmy={addBotToArmy} />
+      <h2>Bot Army</h2>
+      <BotArmy army={BotArmy} removeBotFromArmy={removeBotFromArmy} dischargeBot={dischargeBot} />
     </div>
   );
-}
+};
 
 export default App;
